9bb4bfec550ed99c2c672de6dfa43ba12ca4b512
johnma <ma.jonathan02@gmail.com>
2023-02-13 10:49:17 -0500

    made changes to basicinfo.tsx

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -1,13 +1,7 @@
-/* This is a basic file we are using to familiarize ourselves with you! Simply un-comment these
-below variables and update them with stuff about you. Yeah... I know... the classic basic info
-everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
-
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Jonathan Ma";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "CS + Math";
+let classification : String = "Sophomore";
+let oneExcite : String = "Learning Git and GitHub";
+let anyConcerns : String = "Getting a good grade";
+let funFact : String = "I was born in Houston, Texas";
